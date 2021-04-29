9101573b138ff3a59467ebb848c4cc9107cfd23d
alexmt <al375846@uji.es>
2021-04-29 10:48:48 +0200

    inicio notificaciones

--- src/components/login/LoginModal.tsx
+++ src/components/login/LoginModal.tsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from 'react'
 import { createPortal } from "react-dom"
 import { Alert, Button, Form, Modal } from 'react-bootstrap'
+import {isChrome, isSafari, isFirefox, isEdge} from 'react-device-detect'
 
 import api from '../../api/Api'
 import CredentialsContext from '../../contexts/CredentialsContext'
+import axios from 'axios'
 
 export interface IncidenceModalProps {
     show: boolean,
@@ -36,6 +38,50 @@ export const doLogout = async () => {
     localStorage.removeItem('token')
     localStorage.removeItem('username')
     localStorage.removeItem('usertype')
+    localStorage.removeItem('onesignal')
+}
+
+export const doOneSignalLogin = async () => {
+
+    let devicetype = -1
+    if (isChrome)
+        devicetype = 5
+    else if (isFirefox)
+        devicetype = 8
+    else if (isEdge)
+        devicetype = 4
+    else if (isSafari)
+        devicetype = 7
+
+    if (devicetype === -1)
+        return
+
+    const onesignaldata = {
+        app_id: '577d1044-4e8f-423a-b22d-1eedfff41a75',
+        language: navigator.language,
+        device_type: devicetype,
+        game_version: 0.8
+    }
+
+    await axios.post('https://onesignal.com/api/v1/players', onesignaldata, {
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }).then(response => {
+        localStorage.setItem('onesignal', response.data.id)
+    })
+
+    console.log(localStorage.getItem('onesignal'))
+
+    const logdata = {
+        onesignal: localStorage.getItem('onesignal')
+    }
+
+    await api.post('api/notification', logdata, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+    })
 }
 
 const LoginModal = (props: IncidenceModalProps) => {
@@ -61,6 +107,8 @@ const LoginModal = (props: IncidenceModalProps) => {
             setUsername('')
             setPassword('')
         }).catch(() => {})
+
+        await doOneSignalLogin()
     }
 
     const handleVisible = () => {

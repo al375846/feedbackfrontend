a30c475f8ecfdaaf6afc32a011620530c242ee70
al375846 <al375846@uji.es>
2021-05-12 10:45:57 +0200

    refactor [apprentice profile] : improved module

--- src/modules/profiles/apprentice/ApprenticeProfile.tsx
+++ src/modules/profiles/apprentice/ApprenticeProfile.tsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { FunctionComponent, useContext } from 'react'
 import { Tab, Tabs } from 'react-bootstrap'
+import CredentialsContext from '../../../contexts/CredentialsContext'
 
 import HistoryInfo from '../HistoryInfo'
 import ProfileInfo from '../ProfileInfo'
 
-const ApprenticeProfile = () => {
+interface ApprenticeProfileProps {
+
+}
+
+const ApprenticeProfile: FunctionComponent<ApprenticeProfileProps> = () => {
+
+    const credentials = useContext(CredentialsContext);
+
+    if (!credentials.token)
+        return ( 
+            <div style={{textAlign: 'center'}}>
+                <h1>
+                    Please Login or Register
+                </h1>
+            </div> 
+        )
+
     return (
         <div>
             <div>
-            <Tabs fill defaultActiveKey="profileap" id="apprentice-profile">
-                <Tab eventKey="profileap" title="Profile">
-                    <ProfileInfo />
-                </Tab>
-                <Tab eventKey="historyap" title="History">
-                    <HistoryInfo />
-                </Tab>
-            </Tabs>
+                <Tabs fill defaultActiveKey="profileap" 
+                    id="apprentice-profile">
+                    <Tab eventKey="profileap" title="Profile">
+                        <ProfileInfo />
+                    </Tab>
+                    <Tab eventKey="historyap" title="History">
+                        <HistoryInfo />
+                    </Tab>
+                </Tabs>
             </div>
         </div>
     )

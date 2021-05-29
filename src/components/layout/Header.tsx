4306d19ccfba5b064208b5f6770db7b5bfcf325c
al375846 <al375846@uji.es>
2021-05-29 17:41:51 +0200

    fix [categories] : fix categories menu styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,23 +8,23 @@ const Header = () => {
     return (
         <div className="header-container">
             <div className="header">
-        <div className="ui secondary pointing menu">
-            <div className="item">
-            <Link to="/" className="item">
-                Home
-            </Link>
-            <Link to="/ranking" className="item">
-                Rankings
-            </Link>
-            <Link to="/suggestion" className="item">
-                Suggestions
-            </Link>
+                <div className="ui secondary pointing menu">
+                    <div className="item">
+                    <Link to="/" className="item">
+                        Home
+                    </Link>
+                    <Link to="/ranking" className="item">
+                        Rankings
+                    </Link>
+                    <Link to="/suggestion" className="item">
+                        Suggestions
+                    </Link>
+                    </div>
+                    <div className="right menu">
+                        <LoggedScreen />
+                    </div>
+                </div>
             </div>
-            <div className="right menu">
-                <LoggedScreen />
-            </div>
-        </div>
-        </div>
         </div>
     )
 }
